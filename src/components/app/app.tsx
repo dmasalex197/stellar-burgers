737dc2529a71bc8f0606065a694611218a0d3bd7
dmasalex <aleksey.danilov@tedo.ru>
2025-01-05 13:45:12 +0300

    create routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,116 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
+import { Routes, Route } from 'react-router-dom';
+import { ProtectedRoute } from '../protected-route/protected-route';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          >
+            <OrderInfo />
+          </Modal>
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          >
+            <IngredientDetails />
+          </Modal>
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          >
+            <OrderInfo />
+          </Modal>
+        }
+      />
+    </Routes>
   </div>
 );
 
---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,3 @@
+type ProtectedRouteProps = { children: React.ReactElement };
+
+export const ProtectedRoute = ({ children }: ProtectedRouteProps) => children;
